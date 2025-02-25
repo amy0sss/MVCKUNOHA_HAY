f054ce1d3337d882c36450500a5a1ac38e1d807a
Yosss <juliusjrclastimosa@gmail.com>
2025-02-25 15:46:08 +0800

    done
    Adding Migration and Update Database

--- Data/MyAppContext.cs
+++ Data/MyAppContext.cs
@@ -6,6 +6,8 @@ namespace MVCKUNOHA_HAY.Data
     public class MyAppContext : DbContext
     {
         public MyAppContext(DbContextOptions<MyAppContext>options) : base(options){}
+
+        // Set Database using the Item model class
         public DbSet<Item> Items { get; set; }
     }
 }
